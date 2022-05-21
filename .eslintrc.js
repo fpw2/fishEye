75bcd637a9800f4f320251ca3183ff7ba79c796b
fpw2 <fabien.pergaud@gmail.com>
2022-05-21 11:38:28 +0200

    readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
     "env": {
         "browser": true,
-        "commonjs": true,
         "es2021": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaVersion": "latest"
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
     "rules": {
-        "no-undef": "off",
-        "no-unused-vars": "off",
+            "no-undef": "off",
+            "no-unused-vars": "off"  
     }
 }
